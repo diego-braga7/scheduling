21670ac62d24e288f3eb55783bccff68a5deddf1
Diego Braga <diego.braga@gmail.com>
2023-10-08 16:22:34 -0300

    config and enable basic auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
 
 
 @Controller()
@@ -7,6 +8,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @UseGuards(AuthGuard('basic'))
   getHello(): string {
     return this.appService.getHello();
   }
---
+++ src/auth/auth-basic.strategy.ts
@@ -0,0 +1,26 @@
+import { BasicStrategy as Strategy } from 'passport-http';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class BasicStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private readonly configService: ConfigService,
+    ) {
+        super({
+            passReqToCallback: true
+        });
+    }
+
+    //TODO passar essa validação para o banco de dados.
+    public validate = async (req, username, password): Promise<boolean> => {
+        if (
+            this.configService.get<string>('HTTP_BASIC_USER') === username &&
+            this.configService.get<string>('HTTP_BASIC_PASS') === password
+        ) {
+            return true;
+        }
+        throw new UnauthorizedException();
+    }
+}
